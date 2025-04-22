c562493a7860b69107b871fc092a1d54d9d49557
Joe Fuller <Joseph.tgfuller@gmail.com>
2025-04-21 21:01:04 -0400

    Added code for this leetcode problem. Changed the logic to look at difference between key and value instead of checking even/odd

--- Rabbits in Forest/main.cpp
+++ Rabbits in Forest/main.cpp
@@ -1,14 +1,54 @@
-//
-//  main.cpp
-//  Rabbits in Forest
-//
-//  Created by Joe Fuller on 4/21/25.
-// see if this pushes and works to GIT
-
 #include <iostream>
+#include <string>
+#include <unordered_map>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+int numRabbits(std::vector<int>& answers)
+{
+    
+    std::unordered_map<int, int>response;
+    int minimum_number = 0;
+    
+    for (int i = 0; i < answers.size(); ++i)
+    {
+        response[answers[i]] += 1;
+    }
+    
+    if (response.size() == 1)
+    {
+       return minimum_number = answers[0] + 1;
+        
+    }
+    
+    for (const auto& pair : response)
+    {
+        if (pair.first < pair.second)
+        {
+            if (pair.second - pair.first == 1 || pair.first == 0)
+            {
+                minimum_number += pair.second;
+            }
+            
+            else
+            {
+                minimum_number += pair.second+1;
+            }
+        }
+        else
+        {
+            minimum_number += pair.first+1;
+        }
+    }
+    
+    return minimum_number;
+}
+
+
+int main()
+{
+    std::vector<int>answers = {1, 1, 2};
+    int result = numRabbits(answers);
+
+    return result;
 }
